c821506ba2324b9252c114deda93a02ed244bde9
John William Miones <johnwilliam.miones@cit.edu>
2022-12-06 20:26:31 +0800

    Add body

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Body from './Components/Navbar/Body';
 import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
     <div className = "Navbar">
       <Navbar />
+      <Body />
     </div>
   );
 }
