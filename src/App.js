cbae9160b4d6998720b311d66dd6ce7fd053b7cd
Kent Stephen Sumalinog <kentstephen.sumalinog@cit.edu>
2022-12-07 21:48:52 +0800

    Add App

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+// import Body from './Components/Navbar/Body';
+// import Navbar from './Components/Navbar/Navbar';
+// import Faq from "./Components/FAQ/Faq";
+// import Settings from "./Components/Settings/Settings.js";
+import LandingPage from "./Components/LandingPage/LandingPage.js";
+// import MenteeProfile from "./Components/MenteeProfile/MenteeProfile.js";
+
+function App() {
+  return (
+    <div className = "landingPage">
+      <LandingPage/>
+    </div>
+  );
+}
+
+export default App;
