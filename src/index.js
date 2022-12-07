f12c023fb2326487c18bd89a4af9da36adcfdc3a
Sean Venz Quijano <seanvenz.quijano@cit.edu>
2022-12-07 20:28:39 +0800

    Add Settings asset

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Settings from './Settings/Settings';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
