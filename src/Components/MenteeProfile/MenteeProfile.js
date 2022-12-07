411bb511b4e3ecc8f59cd0a7cae3404de0a10655
Estrella Abueva <estrella.abueva@cit.edu>
2022-12-08 01:33:45 +0800

    MenteeProfile initial design

--- src/Components/MenteeProfile/MenteeProfile.js
+++ src/Components/MenteeProfile/MenteeProfile.js
@@ -1,19 +1,45 @@
 import mentorrantHighReso from "./assets/mentorrantHighReso.png";
 import home from './assets/home.png';
-import rectangle56 from './assets/rectangle56.png';
+import pen from "./assets/pen.png";
+import settings from "./assets/settings.png";
 
-import './Assets.css';
+import './MenteeProfile.css';
 
 const MenteeProfile = () => {
     return (
         <div className="general-profile-mente">
-            <img className="image" src={rectangle56}/>
-            
             <figure className="home_button">
                 <img src={home} alt="home_button"  />
             </figure>
-        
-        
+            
+            <nav className="navbar">
+                <img className ="logo" alt="" src= {mentorrantHighReso} />
+                <h1>Mentorrant</h1>
+                <div className="links">
+                    <a href="https://www.youtube.com/">Mentor</a>
+                    <a href="https://www.facebook.com/">Mentee</a>
+                    <a href="https://www.instagram.com/"><img className = "pen" src = {pen} alt = "pen"/></a>
+                    <a href="https://www.w3schools.com/css/"><img className = "settings" src = {settings} alt = "settings" /></a>
+                </div>
+            </nav>
+            
+            {/* <div className="profile">
+                <div className="profile-picture">
+                    <img className="profile-picture" src="https://www.w3schools.com/howto/img_avatar.png" alt="profile-picture" />
+                    </div>
+                <div className="profile-info">
+                    <h1>John Doe</h1>
+                    <h2>Student</h2>
+                    <h3>University of Waterloo</h3>
+                    <h4>Computer Science</h4>
+                    <h5>Year 2</h5>
+                    <h6>
+                        <a href="https://www.facebook.com/">Facebook</a>
+                        <a href="https://www.instagram.com/">Instagram</a>
+                        <a href="https://www.youtube.com/">Youtube</a>
+                    </h6>
+                </div>
+            </div> */}
         </div>
     );
 }
