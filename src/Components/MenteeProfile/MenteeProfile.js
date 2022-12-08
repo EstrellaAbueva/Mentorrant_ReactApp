f08bda93bff7486ef4d41c119a0561d8bee5e365
Estrella Abueva <estrella.abueva@cit.edu>
2022-12-08 21:11:36 +0800

    Changes for MenteeProfile

--- src/Components/MenteeProfile/MenteeProfile.js
+++ src/Components/MenteeProfile/MenteeProfile.js
@@ -2,7 +2,7 @@ import mentorrantHighReso from "./assets/mentorrantHighReso.png";
 import home from './assets/home.png';
 import pen from "./assets/pen.png";
 import settings from "./assets/settings.png";
-import aj from "./assets/profileAj.png";
+import ferson from "./assets/ferson.png";
 
 import './MenteeProfile.css';
 
@@ -10,12 +10,13 @@ const MenteeProfile = () => {
     return (
         <div className="general-profile-mente">
             { <figure className="home_button">
+                <a href="https://www.youtube.com/">
                 <img src={home} alt="home_button"  />
+                </a>
             </figure> }
 
-
-        <img className ="aj" alt="" src= {aj} />
-
+        <img className ="aj" alt="" src= {ferson} />
+        
         <div className = "card">
                 <div className="links">
                     <a href="https://www.youtube.com/">Mentors</a>
@@ -24,18 +25,20 @@ const MenteeProfile = () => {
                     <a href="https://www.w3schools.com/css/"><img className = "settings" src = {settings} alt = "settings" /></a>
                 </div>
 
-                <h1>Adrian Jay Barcenilla</h1>
+                <h1>ADRIAN JAY BARCENILLA</h1>
 
                 <div className="details">
                     <p>Course: BSCS</p>
                     <p>Email : aj@gmail.com</p>
                     <p>Contact Number: 0999999999</p>
-                    <p>Address: wala lang street c.c </p>
+                    <p>Address: Wala Lang Street Cebu City</p>
                     <p>City: Cebu</p>
                     <p>Zip Code: 6000</p>
                 </div>
 
-                <p><button onclick="window.location.href='https://www.google.com'">Book Mentor</button></p>
+                <button>
+                    Book Mentor
+                </button>
         </div>
 
         </div>
