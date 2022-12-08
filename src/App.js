4c388f79fb49383b124c936a0785411860f3c574
John William Miones <johnwilliam.miones@cit.edu>
2022-12-08 22:29:50 +0800

    Add Ourteam part

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Body from './Components/Navbar/Body';
 import Faq from "./Components/FAQ/Faq";
 import { BrowserRouter as Router ,Route , Routes } from 'react-router-dom';
 import NavbarLink from './Components/Navbar/NavbarLink';
+import OurTeam from './OurTeam';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Router>
       <NavbarLink/>
         <Routes>
+          <Route path='/OurTeam' element = { <OurTeam/>} />
           <Route path='/body' element = { <Body/> } />
           <Route path='/faq' element = { <Faq/> } />
           </Routes>
