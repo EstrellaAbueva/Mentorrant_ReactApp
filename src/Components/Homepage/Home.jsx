c24b4d579d5628cfa6aabe9a3142bfe641c28f47
Andre Lennard S. Uy <andrelennard.uy@cit.edu>
2022-12-09 10:22:22 +0800

    Add home page

--- src/Components/Homepage/Home.jsx
+++ src/Components/Homepage/Home.jsx
@@ -1,4 +1,10 @@
 import React from "react";
+import './Home.css';
+import matchMentor from "./img/matchMentor.png";
+import session from "./img/session.png";
+import ebooks from "./img/ebooks.png";
+import settings from "./img/settings.png";
+
 
 const Home = () => {
     return(
@@ -6,10 +12,12 @@ const Home = () => {
             <div class="container text-center">
                 <div class="row">
                     <div class="col">
+                        <img src={matchMentor} class="photo"/>  
                         <h1 class="display-5">Match a Mentor</h1>
                         <button type="button" class="btn btn-outline-primary btn-lg">Match</button>
                     </div>
                     <div class="col">
+                        <img src={session} class="photo"/>
                         <h1 class="display-5">Session</h1>
                         <button type="button" class="btn btn-outline-primary btn-lg">Session</button>
                     </div>
@@ -19,10 +27,12 @@ const Home = () => {
                 <br></br>
                 <div class="row">
                     <div class="col">
+                        <img src={ebooks} class="photo" />
                         <h1 class="display-5">Available Ebooks</h1>
                         <button type="button" class="btn btn-outline-primary btn-lg">Ebooks</button>
                     </div>
                     <div class="col">
+                        <img src={settings} class="photo" />
                         <h1 class="display-5">Settings</h1>
                         <button type="button" class="btn btn-outline-primary btn-lg">Settings</button>
                     </div>
