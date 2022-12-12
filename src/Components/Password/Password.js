3643b0b29e21374f562e573ffba0cb54e501c786
Sean Venz Quijano <seanvenz.quijano@cit.edu>
2022-12-12 21:34:50 +0800

    Modify stuff

--- src/Components/Password/Password.js
+++ src/Components/Password/Password.js
@@ -6,11 +6,13 @@ const Password =() => {
         <div className="password">
             <hr />
             <form className="submit">
+
                 <h1 className="old-password">ENTER OLD PASSWORD:</h1>
-                <input type = "text" id = "old-password" name = "old-password" placeholder="Enter Old Password" required />
+                <input type = "password" id = "old-password" name = "old-password" placeholder="Enter Old Password" required className="old-password" />
 
                 <h1 className="new-password">ENTER NEW PASSWORD:</h1>
-                <input type = "text" id = "new-password" name = "new-password" placeholder="Enter New Password" />
+                <input type = "password" id = "new-password" name = "new-password" placeholder="Enter New Password" required className="new-password" />
+                
             </form>
             <button>
                 Submit
